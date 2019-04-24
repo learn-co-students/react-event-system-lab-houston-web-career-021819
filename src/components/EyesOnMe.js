11bcb615c81149fb952a67d0c996dbce4b062f3a
github username <github email address>
2019-04-23 21:33:17 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+export default class EyesOnMe extends Component {   // In JS, one element can only have one eventListener?
+    
+     focus = () =>{
+         console.log('Good!')
+     }
+
+     blur = () => {
+         console.log('Hey! Eyes on me!')
+     }
+
+    render(){
+        return(
+            <button onFocus = {this.focus} onBlur = {this.blur}/>
+        )
+    }
+}
